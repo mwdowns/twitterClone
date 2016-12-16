5f45a6fd893f0cd2c2d40a0eb39bf4bd3c0c4fee
Matthew Downs <mwdowns@gmail.com>
2016-12-16 18:17:20 -0500

    adds a lot of new features

--- public/frontend.js
+++ public/frontend.js
@@ -8,17 +8,23 @@ app.config(function($stateProvider, $urlRouterProvider) {
       templateUrl: 'worldtimeline.html',
       controller: 'WorldTimeLineController'
     })
-    // .state({
-    //   name: 'usertimeline',
-    //   url: '/usertimeline/{username}',
-    //   templateUrl: 'usertimeline.html',
-    //   controller: 'UserTimeLineController'
-    // })
     .state({
       name: 'profilepage',
       url: '/profile/{username}',
       templateUrl: 'profile.html',
       controller: 'ProfileController'
+    })
+    .state({
+      name: 'signup',
+      url: '/signup',
+      templateUrl: 'signup.html',
+      controller: 'SignupController'
+    })
+    .state({
+      name: 'login',
+      url: '/login',
+      templateUrl: 'login.html',
+      controller: "LoginController"
     });
 
   $urlRouterProvider.otherwise('/worldtimeline');
@@ -27,6 +33,29 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
 app.factory('tweeterService', function($http, $cookies, $rootScope, $state) {
   var service = {};
+
+  $rootScope.tweetClicked = false;
+
+  if (!$cookies.getObject('cookie_data')) {
+    // $rootScope.username = 'Guest';
+    // $rootScope.followers = [];
+    $rootScope.loggedIn = false;
+  }
+  else {
+    var cookie = $cookies.getObject('cookie_data');
+    $rootScope.username = cookie.username;
+    $rootScope.following = cookie.following;
+    $rootScope.auth_token = cookie.token;
+    $rootScope.loggedIn = true;
+  }
+  // Single logout handler that reinitiallizes the rootScope variables.
+  $rootScope.logout = function() {
+    $cookies.remove('cookie_data');
+    // $rootScope.username = 'Guest';
+    $rootScope.auth_token = null;
+    $rootScope.loggedIn = false;
+    $state.go('worldtimeline');
+  };
 
   service.getWorldTweets = function() {
     return $http.get('/worldtimeline');
@@ -37,8 +66,8 @@ app.factory('tweeterService', function($http, $cookies, $rootScope, $state) {
     return $http.get(url);
   };
 
-  service.Tweet = function(tweet) {
-    data = {tweet: tweet, username: 'mwdowns'};
+  service.Tweet = function(tweet, user) {
+    data = {tweet: tweet, username: user};
     return $http.put('/tweet', data);
   };
 
@@ -47,11 +76,67 @@ app.factory('tweeterService', function($http, $cookies, $rootScope, $state) {
     return $http.get(url);
   };
 
+  service.signup = function(formData) {
+    return $http.post('/signup', formData);
+  };
+
+  service.login = function(formData) {
+    return $http({
+      method: 'POST',
+      url: '/login',
+      data: formData
+    })
+    .then(function(response) {
+      var data = response.data;
+      console.log('this is the data for the cookie, ', data);
+      $cookies.putObject('cookie_data', data);
+      $rootScope.username = data.username;
+      $rootScope.following = data.following;
+      $rootScope.auth_token = data.token;
+    })
+    .catch(function(err) {
+      console.log('this sucks');
+      $rootScope.wronglogin = true;
+    });
+
+  };
+
+  service.followUser = function(formData) {
+    return $http.post('/follow', formData);
+  };
+
+  service.unfollowUser = function(formData) {
+    return $http.post('/unfollow', formData);
+  };
+
+  service.searchTweet = function(searchParams) {
+    return $http.post('/search', searchParams);
+  };
+
+  service.retweet = function(formData) {
+    return $http.post('/retweet', formData);
+  };
+
+  service.favorite = function(formData) {
+    return $http.post('/favorite', formData);
+  };
+
   return service;
 
 });
 
-app.controller('WorldTimeLineController', function($scope, tweeterService, $stateParams, $state) {
+function getTweet(tweet) {
+  console.log(tweet);
+  if (tweet.tweetClicked) {
+    tweet.tweetClicked = false;
+  } else {
+    var user = tweet.user;
+    var tweetID = tweet.tweetID;
+    tweet.tweetClicked = true;
+  }
+}
+
+app.controller('WorldTimeLineController', function($scope, tweeterService, $stateParams, $state, $cookies, $rootScope) {
   tweeterService.getWorldTweets()
   .success(function(tweets) {
     $scope.tweets = tweets;
@@ -60,43 +145,246 @@ app.controller('WorldTimeLineController', function($scope, tweeterService, $stat
   .error(function(err) {
     console.log('you got an error: ', err);
   });
+
+  $scope.getTweet = getTweet;
+
+  // $scope.retweet = retweet;
+
+  $scope.goToUser = function(user) {
+    $state.go('profilepage', {username: user});
+  };
+
+  $scope.retweet = function(tweet) {
+    console.log(tweet.tweetID);
+    console.log($rootScope.username);
+    var formData = {
+      tweetID: tweet.tweetID,
+      tweetAuthor: tweet.user,
+      tweet: tweet.tweet,
+      retweetedBy: $rootScope.username
+    };
+    tweeterService.retweet(formData)
+    .then(function(data) {
+      console.log('success!', data);
+    })
+    .catch(function(err) {
+      console.log('there was an error', err);
+    });
+  };
+
+  $scope.favorite = function(tweet) {
+    console.log(tweet.tweetID);
+    console.log($rootScope.username);
+    var formData = {
+      tweetID: tweet.tweetID,
+      tweetAuthor: tweet.user,
+      tweet: tweet.tweet,
+      favoritedBy: $rootScope.username
+    };
+    tweeterService.favorite(formData)
+    .then(function(data) {
+      console.log('success!', data);
+    })
+    .catch(function(err) {
+      console.log('there was an error', err);
+    });
+  };
+
 });
 
-app.controller('ProfileController', function($scope, tweeterService, $stateParams, $state) {
+app.controller('ProfileController', function($scope, tweeterService, $stateParams, $state, $cookies, $rootScope) {
+  $scope.followerscliked = false;
+  $scope.followingclicked = false;
+
   $scope.username = $stateParams.username;
-  tweeterService.getUserTimeLine($scope.username)
-  .success(function(tweets) {
-    $scope.tweets = tweets;
-  })
-  .error(function(err) {
-    console.log('you got an error: ', err);
-  });
+
+  $scope.showMyTimeLine = function() {
+    if($scope.showmytimeline) {
+      $scope.showmytimeline = false;
+    }
+    else {
+      $scope.showmytimeline = true;
+      tweeterService.getUserTimeLine($scope.username)
+      .success(function(tweets) {
+        $scope.tweets = tweets;
+      })
+      .error(function(err) {
+        console.log('you got an error: ', err);
+      });
+    }
+  };
+
 
   tweeterService.userProfile($scope.username)
   .success(function(data) {
+    console.log('this is the scope.username', $scope.username);
     console.log('this is the data i got, ', data);
-    $scope.username = data._id;
+    $scope.username = data.id;
     $scope.followers = data.followers;
     $scope.following = data.following;
+    $scope.favorites = data.favorites;
   })
   .error(function(err) {
     console.log('you got an error: ', err);
   });
 
+  $scope.clickingFollowers = function() {
+    if (!$scope.followerscliked) {
+      $scope.followerscliked = true;
+    } else {
+      $scope.followerscliked = false;
+    }
+  };
+
+  $scope.clickingFollowing = function() {
+    if (!$scope.followingclicked) {
+      $scope.followingclicked = true;
+    } else {
+      $scope.followingclicked = false;
+    }
+  };
+
+  $scope.getTweet = getTweet;
+
+  $scope.goToUser = function(user) {
+    $state.go('profilepage', {username: user});
+  };
+  if(!$rootScope.loggedIn) {
+    console.log('not logged in!');
+  }
+  else {
+    console.log('rootscope following, ', $rootScope.following.length);
+    if ($rootScope.following.length === 0) {
+      $scope.follow = function() {
+        console.log('you hit follow');
+        var formData = {
+          usertofollow: $stateParams.username,
+          userfollowing: $rootScope.username
+        };
+        console.log(formData);
+        tweeterService.followUser(formData);
+      };
+    }
+    else {
+      if ($rootScope.username != $stateParams.username) {
+        console.log($rootScope.username);
+        var index = $rootScope.following.indexOf($stateParams.username);
+        console.log('hi');
+        if (index === -1) {
+          $scope.follow = function() {
+            var formData = {
+              usertofollow: $stateParams.username,
+              userfollowing: $rootScope.username
+            };
+            tweeterService.followUser(formData);
+          };
+        }
+        else {
+          $scope.isfollowing = true;
+          console.log('this is scope.following inside else, ', $scope.following);
+          $scope.unfollow = function() {
+            var formData = {
+              usertofollow: $stateParams.username,
+              userfollowing: $rootScope.username
+            };
+            tweeterService.unfollowUser(formData);
+          };
+        }
+      }
+      else {
+        $scope.isme = true;
+      }
+    }
+  }
+  console.log('this is isme, ', $scope.isme);
 });
 
-app.controller('TweetController', function($scope, tweeterService, $stateParams, $state) {
+app.controller('TweetController', function($scope, tweeterService, $stateParams, $state, $cookies, $rootScope) {
   $scope.submitTweet = function(tweet) {
-    tweeterService.Tweet($scope.tweet)
-    .success(function(data) {
-      console.log('success!', data);
-    })
-    .error(function(err) {
-      console.log('you got an error: ', err);
-    });
+    if ($rootScope.username) {
+      var user = $rootScope.username;
+      tweeterService.Tweet($scope.tweet, user)
+      .success(function(data) {
+        console.log('success!', data);
+        // $state.go('profilepage', {username: user});
+      })
+      .error(function(err) {
+        console.log('you got an error: ', err);
+      });
+      $state.reload();
+      $scope.tweet = '';
+      // $state.go('profilepage', {username: user});
+    }
+    else {
+      console.log('this messed up somehow');
+    }
+  // $state.go('profilepage', {username: $stateParams.username});
+  };
+
+  $scope.goToUser = function(user) {
+    $state.go('profilepage', {username: user});
+  };
+
+  $scope.goToWorldTimeLine = function() {
+    console.log('you clicked go to world timeline');
+    $state.go('worldtimeline');
+  };
+
+  $scope.goToLogin = function() {
+    $state.go('login');
+  };
+
+  $scope.goToSignUp = function() {
+    $state.go('signup');
+  };
+
+});
+
+app.controller('SignupController', function($scope, tweeterService, $stateParams, $state, $cookies, $rootScope) {
+  $scope.signupSubmit = function() {
+    if ($scope.password != $scope.confirmPassword) {
+      $scope.passwordsdontmatch = true;
+    }
+    else {
+      $scope.passwordsdontmatch = false;
+      var formData = {
+        username: $scope.username,
+        password: $scope.password,
+      };
+      tweeterService.signup(formData)
+        .success(function() {
+          $state.go('worldtimeline');
+          console.log('success');
+        })
+        .error(function() {
+          console.log('could not sign up');
+        });
+      $scope.username = '';
+    }
   };
 });
 
-// app.controller('ProfileController', function($scope, tweeterService, $stateParams, $state) {
-//   $scope
-// });
+app.controller('LoginController', function($scope, tweeterService, $stateParams, $state, $cookies, $rootScope) {
+  $scope.login = function() {
+    var formData = {
+      username: $scope.user,
+      password: $scope.password
+    };
+    tweeterService.login(formData)
+      .then(function() {
+        if($cookies.getObject('cookie_data')) {
+          $state.go('profilepage', {username: formData.username});
+          $rootScope.loggedIn = true;
+        }
+        else {
+          $state.go('login');
+        }
+      })
+      .catch(function(err) {
+        console.log('login failed');
+        $scope.wronglogin = true;
+      });
+    $scope.user = '';
+    $scope.password = '';
+  };
+});
--- twitterCloneBackend.js
+++ twitterCloneBackend.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose'),
       bodyParser = require('body-parser'),
       bluebird = require('bluebird'),
       express = require('express'),
-      app = express();
+      app = express(),
+      uuid = require('uuid'),
+      bcrypt = require('bcrypt'),
+      salt = 12;
 
 mongoose.Promise = bluebird;
 app.use(bodyParser.json());
@@ -16,29 +19,33 @@ const User = mongoose.model('User', {
   _id: String,
   password: {type: String, required: true},
   // avatar: binary,
+  token: String,
   following: [String],
-  followers: [String]
+  followers: [String],
+  favorites: [String]
 });
 
 //the tweet schema
 const Tweet = mongoose.model('Tweet', {
-  tweet: {type: String, required: true},
+  tweet: {type: String, minlength: 1, maxlength: 140, required: true},
   date: Date,
   userID: String,
-  favorites: [String],
-  retweets: [String]
+  favorites: Number,
+  favoritedBy: [String],
+  retweets: Number,
+  retweetedBy: [String]
 });
 
 // var theUserId = 'mwdowns';
 
 app.get('/worldtimeline', function(req, res) {
   // World Timeline
-  Tweet.find().limit(20)
+  Tweet.find()
   .then(function(data) {
-    console.log(data);
     var worldtimeline = [];
     data.forEach(function(tweet) {
       worldtimeline.push({
+        tweetID: tweet._id,
         tweet: tweet.tweet,
         date: tweet.date,
         user: tweet.userID
@@ -47,17 +54,91 @@ app.get('/worldtimeline', function(req, res) {
     res.json(worldtimeline);
   })
   .catch(function(err) {
-    console.log('you got an error, ', err);
     res.json({message: 'you got an error', error: err});
   });
 });
+
+app.post('/search', function(req, res) {
+  var params = req.body.data;
+  // Tweet.createIndex({"tweet":"text"});
+  Tweet.find({tweet: {$text: {$search: params}}})
+  .then(function(data) {
+    res.json({message: 'success!', data: data});
+  })
+  .catch(function(err) {
+    res.json({message: 'you got an error', data: err.message});
+  });
+});
+
+app.post('/signup', function(req, res) {
+  var user = req.body.username;
+  var password = req.body.password;
+  bcrypt.hash(password, salt)
+    .then(function(hash) {
+      User.create ({
+        _id: user,
+        password: hash
+      })
+      .then(function(data) {
+        res.json({message: 'success!', data: data});
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.json({message: 'you got an error:', data: err});
+      });
+  });
+});
+
+app.post('/login', function(req, res) {
+  var user = req.body.username;
+  var password = req.body.password;
+  console.log(user, password);
+  User.findById(user)
+  .then(function(data) {
+    console.log(data.password);
+    console.log(password);
+    return [data, bcrypt.compare(password, data.password)];
+  })
+  .spread(function(data, newHash) {
+    //figure out what to do tomorrow. maybe put that if statement up in here.
+    var token = uuid();
+    if (newHash) {
+      console.log('this is the token in the if, ', token);
+      User.update(
+        {_id: data._id},
+        {
+          $set: {
+            token: token
+          }
+        }
+      )
+      .then(function(data) {
+        return;
+      })
+      .catch(function(err) {
+        res.json({message: 'error', err: err});
+      });
+      console.log('this is the token outside, ', token);
+      res.json({message: 'success', username: data._id, following: data.following, token: token});
+    }
+    else {
+      throw ('passwords do not match');
+    }
+  })
+  .catch(function(err) {
+    console.log(err);
+    res.status(401);
+    res.json({message: 'you got an error:', err: err.message});
+    // res.send(err);
+  });
+});
+
 
 app.get('/usertimeline/:username', function(req, res) {
 //My Timeline
   var username = req.params.username;
   User.findById(username)
     .then(function(user) {
-      console.log('got here and theUserId is ', username);
       return Tweet.find({
         userID: {
           $in: user.following.concat([user._id])
@@ -76,15 +157,26 @@ app.get('/usertimeline/:username', function(req, res) {
       res.json(usertimeline);
     })
     .catch(function(err) {
-      console.log('you got an error, ', err);
       res.json({message: 'you got an error', error: err});
     });
+});
+
+app.get('/profile/:username', function(req, res) {
+  // User Profile page
+  var username = req.params.username;
+  console.log('this is the username on the /profile/:username page, ', username);
+  User.findById(username)
+  .then(function(data) {
+    res.json({id: data._id, followers: data.followers, following: data.following, favorites: data.favorites});
+  })
+  .catch(function(err) {
+    res.json({message: 'you got an error', error: err});
+  });
 });
 
 app.put('/tweet', function(req, res) {
   var tweet = req.body.tweet;
   var username = req.body.username;
-  console.log(username + ': ' + tweet);
   // Creates a tweet
   Tweet.create({
     tweet: tweet,
@@ -92,102 +184,143 @@ app.put('/tweet', function(req, res) {
     userID: username
   })
   .then(function(data) {
-    console.log('success! ', data);
     res.json({message: 'success!', data: data});
   })
   .catch(function(err) {
-    console.log('you got an error, ', err);
     res.json({message: 'you got an error', data: err});
   });
 });
 
-app.get('/profile/:username', function(req, res) {
-  // User Profile page
-  var username = req.params.username;
-  User.findById(username)
+app.post('/follow', function(req, res) {
+  var usertofollow = req.body.usertofollow;
+  var userfollowing = req.body.userfollowing;
+  //Updates a user's following list or followers list (change the key after the $push)
+  bluebird.all(
+    [User.update(
+      {_id: userfollowing},
+      {
+        $addToSet: {
+          following: usertofollow
+        }
+      }
+    ),
+    User.update(
+      {_id: usertofollow},
+      {
+        $addToSet: {
+          followers: userfollowing
+        }
+      }
+    )]
+  )
   .then(function(data) {
-    console.log('i got the username and it is ', username);
-    console.log('this is the data', data);
-    res.json(data);
+    res.json({message: 'success', data: data});
   })
   .catch(function(err) {
-    console.log('you got an error, ', err);
-    res.json({message: 'you got an error', error: err});
+    res.json({message: 'you got an error:', data: err.message});
+  });
+});
+
+app.post('/unfollow', function(req, res) {
+  var usertofollow = req.body.usertofollow;
+  var userfollowing = req.body.userfollowing;
+  //Updates a user's following list or followers list (change the key after the $push)
+  bluebird.all([
+    User.update(
+      {_id: userfollowing},
+      {
+        $pull: {
+          following: usertofollow
+        }
+      }
+    ),
+    User.update(
+      {_id: usertofollow},
+      {
+        $pull: {
+          followers: userfollowing
+        }
+      }
+    )]
+  )
+  .then(function(data) {
+    res.json({message: 'success', data: data});
+  })
+  .catch(function(err) {
+    res.json({message: 'you got an error:', data: err.message});
+  });
+});
+
+app.post('/retweet', function(req, res) {
+  var tweetID = req.body.tweetID;
+  var tweetAuthor = req.body.tweetAuthor;
+  var tweetText = req.body.tweet;
+  var retweetedBy = req.body.retweetedBy;
+  bluebird.all([
+    Tweet.update(
+      {_id: tweetID},
+      {
+        $inc: {
+          retweets: 1
+        }
+      }
+    ),
+    Tweet.update(
+      {_id: tweetID},
+      {
+        $addToSet: {
+          retweetedBy: retweetedBy
+        }
+      }
+    )]
+  )
+  .then(function(data) {
+    res.json({message: 'success', data: data});
+  })
+  .catch(function(err) {
+    res.json({message: 'you got an error: ', data: err.message});
+  });
+});
+
+app.post('/favorite', function(req, res) {
+  var tweetID = req.body.tweetID;
+  var tweetAuthor = req.body.tweetAuthor;
+  var tweetText = req.body.tweet;
+  var favoritedBy = req.body.favoritedBy;
+  bluebird.all([
+    Tweet.update(
+      {_id: tweetID},
+      {
+        $inc: {
+          favorites: 1
+        }
+      }
+    ),
+    Tweet.update(
+      {_id: tweetID},
+      {
+        $addToSet: {
+          favoritedBy: favoritedBy
+        }
+      }
+    ),
+    User.update(
+      {_id: favoritedBy},
+      {
+        $addToSet: {
+          favorites: tweetID
+        }
+      }
+    )]
+  )
+  .then(function(data) {
+    res.json({message: 'success', data: data});
+  })
+  .catch(function(err) {
+    res.json({message: 'you got an error: ', data: err.message});
   });
 });
 
 app.listen(3000, function() {
   console.log('listening on port 3000');
 });
-
-//Creates a new user
-// User.create ({
-//   _id: 'mwdowns',
-//   password: '1234'
-// })
-// .then(function(data) {
-//   console.log('success ', data);
-// })
-// .catch(function(err) {
-//   console.log('you got an error: ', err);
-// });
-
-//Updates a user's following list or followers list (change the key after the $push)
-// User.update(
-//   {_id: 'lynn'},
-//   {
-//     $push: { following:
-//       {$each: ['DOM', 'mwdowns']}
-//     }
-//   },
-//   {
-//     upsert: true
-//   }
-// )
-// .then(function(data) {
-//   console.log('success! ', data);
-// })
-// .catch(function(err) {
-//   console.log('there was an error, ', err);
-// });
-
-//Creates a tweet
-// Tweet.create({
-//   tweet: 'hey everyone! i love horses! and Doodins!',
-//   date: new Date(),
-//   userID: 'jeri'
-// })
-// .then(function(data) {
-//   console.log('success! ', data);
-// })
-// .catch(function(err) {
-//   console.log('you got an error, ', err);
-// });
-//
-// // World Timeline
-//
-// Tweet.find().limit(20);
-//
-// // User Profile page
-//
-// bluebird.all([
-//   Tweet.find({ userID: theUserID }).limit(20),
-//   User.findById(theUserId)
-// ])
-// .spread(function(tweets, user) {
-//
-// });
-//
-// My timeline
-
-// User.findById(theUserId)
-//   .then(function(user) {
-//     return Tweet.find({
-//       userID: {
-//         $in: user.following.concat([user._id])
-//       }
-//     });
-//   })
-//   .then(function(tweets) {
-//     // you have the tweets
-//   });
